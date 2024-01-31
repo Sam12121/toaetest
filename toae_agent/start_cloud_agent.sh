03955e08ad09cacaa00e0571fc6d3cb84ff95c17
Sam12121 <smcoolsam44@gmail.com>
2024-01-31 19:38:20 +0530

    Update start_cloud_agent.sh

--- toae_agent/start_cloud_agent.sh
+++ toae_agent/start_cloud_agent.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
+# Start NGINX
+nginx -g "daemon off;" &
+
+# Start other services or commands if needed
 sudo su root -c "service cron start && (echo \"*/5 * * * * /usr/sbin/logrotate /etc/logrotate.d/logrotate.conf\") | crontab -" 2>&1
 
-if [[ "${1#-}" != "$1" ]];
-then
+# Check if the first argument doesn't start with a hyphen
+if [[ "${1#-}" != "$1" ]]; then
     set -- /usr/local/bin/cloud_scanner "$@"
 fi
 
+# Execute the command
 exec "$@"
